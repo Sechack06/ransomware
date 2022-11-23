c4d29fdac6310604435a231e7084a0805d87ad68
Sechack <57826302+Sechack06@users.noreply.github.com>
2022-11-23 23:14:43 +0900

    Delete header directory

--- header/file.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <Windows.h>
-#include "encrypt.h"
-#include "decrypt.h"
-
-void all_files_enc_dec(char* dir, char *key, BOOL mode);
